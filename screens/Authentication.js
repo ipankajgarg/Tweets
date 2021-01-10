6d52142ae5dbba027b5a548d3f57c6ec420f5597
Pankaj garg <gargpankaj901@gmail.com>
2021-01-10 20:14:54 +0530

    authentication

---
+++ screens/Authentication.js
@@ -0,0 +1,21 @@
+import React, {useEffect} from 'react';
+import {View, Text, AsyncStorage} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+function Authentication() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    AsyncStorage.getItem('isLoggedIn', (err, result) => {
+      console.log(err, result);
+      if (!result) {
+        return navigation.navigate('Dashboard');
+      }
+      navigation.navigate('Home');
+    });
+  });
+
+  return <View style={{flex: 1, backgroundColor: 'black'}}></View>;
+}
+
+export default Authentication;
